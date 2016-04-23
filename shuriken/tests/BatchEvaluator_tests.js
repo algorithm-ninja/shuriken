3720cdd7651b0f19b292cfe1138b4bf0c51953e7
William Di Luigi <williamdiluigi@gmail.com>
2016-04-24 00:18:26 +0200

    Improve BatchEvaluator and BatchTestcaseEvaluator unit tests

--- shuriken/tests/BatchEvaluator_tests.js
+++ shuriken/tests/BatchEvaluator_tests.js
@@ -3,9 +3,43 @@
 import test from 'ava';
 import BatchEvaluator from '../evaluators/BatchEvaluator';
 
-//TODO Add tests
-
 test('BatchEvaluator checks job.data is Object', t => {
-    const job = {data: 'string'};
-    t.throws(() => {new BatchEvaluator({}, null);});
+  const job = {
+    data: 'some string'
+  };
+
+  t.throws(() => {
+    new BatchEvaluator(job, null);
+  }, /to be an object/);
+});
+
+test('BatchEvaluator checks missing properties', t => {
+  const job = {};
+  job.data = {
+    submissionFileUri: 'file:///tmp/source.cpp',
+    tcInputFileUriSchema: 'file:///tmp/input.%d.%d.txt',
+    evaluationStructure: [],
+    timeLimit: 1.0,
+    memoryLimit: 256
+  };
+
+  t.throws(() => {
+    new BatchEvaluator(job, null);
+  }, /to have property tcOutputFileUriSchema/);
+});
+
+test('BatchEvaluator checks invalid properties', t => {
+  const job = {};
+  job.data = {
+    submissionFileUri: 'file:///tmp/source.cpp',
+    tcInputFileUriSchema: 'file:///tmp/input.%d.%d.txt',
+    tcOutputFileUriSchema: 'file:///tmp/input.%d.%d.txt',
+    evaluationStructure: [],
+    timeLimit: -1.0,
+    memoryLimit: 256
+  };
+
+  t.throws(() => {
+    new BatchEvaluator(job, null);
+  }, /to be above 0/);
 });
--- shuriken/tests/BatchTestcaseEvaluator_tests.js
+++ shuriken/tests/BatchTestcaseEvaluator_tests.js
@@ -3,9 +3,43 @@
 import test from 'ava';
 import BatchTestcaseEvaluator from '../evaluators/BatchTestcaseEvaluator';
 
-//TODO Add tests
-
 test('BatchTestcaseEvaluator checks job.data is Object', t => {
-    const job = {data: 'string'};
-    t.throws(() => {new BatchTestcaseEvaluator({}, null);});
+  const job = {
+    data: 'some string'
+  };
+
+  t.throws(() => {
+    new BatchTestcaseEvaluator(job, null);
+  }, /to be an object/);
+});
+
+test('BatchTestcaseEvaluator checks missing properties', t => {
+  const job = {};
+  job.data = {
+    submissionFileUri: 'file:///tmp/source.cpp',
+    tcOutputFileUri: 'file:///tmp/input.txt',
+    timeLimit: 1.0,
+    memoryLimit: 256
+  };
+
+  t.throws(() => {
+    new BatchTestcaseEvaluator(job, null);
+  }, /to have property tcInputFileUri/);
+});
+
+test('BatchTestcaseEvaluator checks invalid properties', t => {
+  const job = {};
+  job.data = {
+    submissionFileUri: 'file:///tmp/source.cpp',
+    submissionLanguage: 'some inexistent language',
+    tcInputFileUri: 'file:///tmp/input.txt',
+    tcOutputFileUri: 'file:///tmp/input.txt',
+    evaluationStructure: [],
+    timeLimit: 1.0,
+    memoryLimit: 256
+  };
+
+  t.throws(() => {
+    new BatchTestcaseEvaluator(job, null);
+  }, /to be equals one of Array/);
 });
