f33178616ddfe6dbf1d86ccdd86bf21464e62358
Gabriele Farina <gabr.farina@gmail.com>
2016-06-03 00:38:08 +0200

    ObjectId handling

--- shuriken/rws-connector/dataStore.js
+++ shuriken/rws-connector/dataStore.js
@@ -8,24 +8,31 @@ module.exports = class DataStore {
 
   add(collection, objectId, fields) {
     console.log(`[  +  ] New object in ${collection}, id ${objectId}`);
+
+    if (collection !== 'users') { //FIXME
+      objectId = {'$type': 'oid', '$value': objectId};
+    }
+    should(fields).not.have.property('_id');
+    fields._id = objectId;
+
+    objectId = JSON.stringify(objectId);
+
     if (!_.has(this._data, collection)) {
       this._data[collection] = {};
     }
     should(this._data[collection]).should.not.have.property(objectId);
-    should(fields).not.have.property('_id');
-    fields = _.mapValues(fields, (value) => {
-      if (!_.has(value, '$type') || value.$type !== 'oid') {
-        return value;
-      } else {
-        return value.$value;
-      }
-    });
-    fields._id = objectId;
+
     this._data[collection][objectId] = fields;
   }
 
   remove(collection, objectId) {
     console.log(`[  -  ] Deletion in ${collection}, id ${objectId}`);
+
+    if (collection !== 'users') {
+      objectId = {'$type': 'oid', '$value': objectId};
+    }
+    objectId = JSON.stringify(objectId);
+
     should(this._data).have.property(collection);
     should(this._data[collection]).have.property(objectId);
     this._data[collection] = _.omit(this._data[collection], objectId);
@@ -33,16 +40,15 @@ module.exports = class DataStore {
 
   change(collection, objectId, fields, cleared) {
     console.log(`[  *  ] Change in ${collection}, id ${objectId}`);
+
+    if (collection !== 'users') { //FIXME
+      objectId = {'$type': 'oid', '$value': objectId};
+    }
+    objectId = JSON.stringify(objectId);
+
     should(this._data).have.property(collection);
     should(this._data[collection]).have.property(objectId);
 
-    fields = _.mapValues(fields, (value) => {
-      if (!_.has(value, '$type') || value.$type !== 'oid') {
-        return value;
-      } else {
-        return value.$value;
-      }
-    });
     this._data[collection][objectId] = _.omit(
       _.assign(this._data[collection][objectId], fields), cleared);
   }
@@ -55,10 +61,11 @@ module.exports = class DataStore {
     if (_.isNil(key)) {
       return _.find(this._data[collection], true);
     } else if (key === '_id') {
+      value = JSON.stringify(value);
       return _.get(this._data[collection], value, undefined);
     } else {
       return _.find(this._data[collection], (fields) => {
-        return (_.get(fields, key, undefined) === value);
+        return _.isEqual(_.get(fields, key, undefined), value);
       });
     }
   }
@@ -71,10 +78,11 @@ module.exports = class DataStore {
     if (_.isNil(key)) {
       return _.map(this._data[collection], (obj) => {return obj;});
     } else if (key === '_id') {
+      value = JSON.stringify(value);
       return _.get(this._data[collection], value, undefined);
     } else {
       return _.filter(this._data[collection], (fields) => {
-        return (_.get(fields, key, undefined) === value);
+        return _.isEqual(_.get(fields, key, undefined), value);
       });
     }
   }
