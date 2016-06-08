49df88803864c1d0121391f67522ec40fe137f9e
Gabriele Farina <gabr.farina@gmail.com>
2016-06-08 10:23:29 +0200

    Publish user profile field

--- shuriken-web/imports/api/users.js
+++ shuriken-web/imports/api/users.js
@@ -4,5 +4,6 @@
  * Publish all users.
  */
 Meteor.publish('Users', function () {
-  return Meteor.users.find({}, {fields: {username: true, roles: true}});
+  return Meteor.users.find({}, {
+      fields: {username: true, roles: true, profile: true}});
 });
